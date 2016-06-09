82d9e718bb8b42764bc9c7646083af7f0e6ea588
jmml <jmmartinluque@gmail.com>
2016-06-09 11:45:02 +0200

    Intento 2

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function procesa() {
    var minimo = 2;
    var total_diputados = 350;
    var metodo = dhondt;
-   var tipo_circ = provincia;
-   var corte = parseFloat(document.getElementById('corte').value.replace(",", "."), 10);
+   var tipo_circ = "provincia";
+   var corte = 3;
    var trasvases = (document.getElementById('iu_pod').checked ? [["Unidos Podemos", ["Podemos", "EN COMÚ", "Compromís-Podemos", "En Marea", "IU-UPeC"]]] : (document.getElementById('pod_conf').checked ? [["Podemos", ["EN COMÚ", "Compromís-Podemos", "En Marea"]]] : []));
    var escanyos = getEscanyos(minimo,[[["Ceuta", "Melilla"], 1]], total_diputados, tipo_circ == "comunidad");
 
